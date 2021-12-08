25642cf7f96e8d695abe84bf00273725e71bd235
Addison Calles <addison.calles@clearcheck.us>
2021-12-08 15:58:18 -0600

    alpha-v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,98 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+
+import { EventListener } from "./eventListener.class.js";
+
+
+export class Game {
+  private _play = false;
+  private _level = 0;
+  private _context: any = {};
+  private _points = 0;
+  private _mainRunner: any;
+  private _canvas: any = {};
+  private _gameover = false;
+  private _playEvent = new EventListener();
+  private _preloadEvent = new EventListener();
+  private _gameOverEvent = new EventListener();
+  private _renderEvent = new EventListener();
+  private _nextLevelEvent = new EventListener();
+  private _fps = 1;
+  private _fpsCounter = 0;
+  private _time: number = 0;
+  private _initTime: number = 0;
+  private _lastTime: number = 0;
+  private _controls = {
+    fire: new EventListener(),
+    move: new EventListener(),
+    jump: new EventListener(),
+    
   }
-  return a + b;
-};
+  constructor(canvas?: HTMLCanvasElement) {
+    this._canvas = canvas;
+    this._context = canvas?.getContext('2d');
+    this._preloadEvent.emit(this);
+  }
+  render() {
+    if (!this._context) return;
+    this._renderEvent.emit(this);
+    if (this.isPlay) {
+
+    }
+  }
+  _initMouseEvents() {
+    const gameRef = this;
+    this._canvas?.addEventListener('click', function (event: any) {
+        gameRef._controls.fire.emit({context: gameRef, event: event});
+    });
+    this._canvas?.addEventListener('mousemove', function (event: any) {
+        gameRef._controls.move.emit({context: gameRef, event: event});
+    });
+  }
+  clearCanvas() {
+    if (this._context && this._canvas) this._context?.clearRect(0, 0, this._canvas?.width, this._canvas?.height);
+  }
+  play() {
+    this._play = true;
+  }
+  nextLevel(sleepOffset = 0) {
+    this._level++;
+    setTimeout(() => {
+
+    }, sleepOffset);
+  }
+
+  start() {
+    this._initTime = new Date().getTime();
+    this._mainRunner = setInterval(() => {
+      this.clearCanvas();
+      this.render();
+      const now = new Date().getTime();
+      const milliSecondsDif = (now) - this._lastTime;
+      this._time = (+((now - this._initTime) / 1000).toFixed(0));
+
+      if (milliSecondsDif >= 1000) {
+        this._lastTime = new Date().getTime();
+        this._fps = this._fpsCounter;
+        this._fpsCounter = 0;
+      }
+      this._fpsCounter++;
+    }, 1000 / 60)
+  }
+
+  stop() {
+    clearInterval(this._mainRunner);
+  }
+
+  incrementPoints(amount:number){
+    return this._points += amount;
+  }
+  get gameOverEvent() { return this._gameOverEvent; }
+  get nextLevelEvent() { return this._nextLevelEvent; }
+  get playEvent() { return this._playEvent; }
+  get gameOver() { return this._gameover; }
+  get points() { return this._points; }
+  get level() { return this._level; }
+  get isPlay() { return this._play; }
+  get time(){
+    return this._time;
+  }
+}
---
+++ test/drawable.class.test.ts
@@ -0,0 +1,60 @@
+import { Drawable } from "../src/drawable.class";
+
+const canvas = { getContext: () => { property: 'context' }, width: 0, height: 0 };
+test('public Fields [x,y,x2,y2,canvas,context,rotation,offset,width,height]', () => {
+    const draw = new Drawable(canvas, 1, 2, 30, 33);
+    expect(draw).toHaveProperty('x', 1);
+    expect(draw).toHaveProperty('y', 2);
+    expect(draw).toHaveProperty('x2', 31);
+    expect(draw).toHaveProperty('y2', 35);
+    expect(draw).toHaveProperty('canvas', canvas);
+    expect(draw).toHaveProperty('context', canvas.getContext());
+    expect(draw).toHaveProperty('rotation', 0);
+    expect(draw).toHaveProperty('offset.x', 0);
+    expect(draw).toHaveProperty('offset.y', 0);
+    expect(draw).toHaveProperty('width', 30);
+    expect(draw).toHaveProperty('height', 33);
+});   
+
+test('set offset', () => {
+    const draw = new Drawable(canvas, 5, 5, 30, 30);
+    draw.offset = {x: 15, y: 20};
+    expect(draw).toHaveProperty('offset.x', 15);
+    expect(draw).toHaveProperty('offset.y', 20);
+    expect(draw).toHaveProperty('x', 20);
+    expect(draw).toHaveProperty('y', 25);
+});   
+test('move', () => {
+    const draw = new Drawable(canvas, 5, 5, 30, 30);
+    draw.offset = {x: 15, y: 20};
+    draw.move(5,5)
+    expect(draw).toHaveProperty('x', 25);
+    expect(draw).toHaveProperty('y', 30);
+});   
+test('rotate(grades)', () => {
+    const draw = new Drawable(canvas, 5, 5, 30, 30);
+    draw.offset = {x: 15, y: 20};
+    draw.rotate(1);
+    expect(draw).toHaveProperty('rotation', 1);
+    draw.rotate(200);
+    expect(draw).toHaveProperty('rotation', 201);
+    draw.rotate(-200);
+    expect(draw).toHaveProperty('rotation', 1);
+});   
+
+test('setPos(x,y)', () => {
+    const draw = new Drawable(canvas, 5, 5, 30, 30);
+    draw.offset = {x: 15, y: 20};
+    draw.setPos(10, 20);
+    expect(draw).toHaveProperty('x', 25);
+    expect(draw).toHaveProperty('y', 40);
+})
+
+test('centerOffset()', () => {
+    const draw = new Drawable(canvas, 5, 10, 30, 40);
+    draw.centerOffset();
+    expect(draw).toHaveProperty('offset.x', 30/2);
+    expect(draw).toHaveProperty('offset.y', 40/2);
+    expect(draw).toHaveProperty('x', 5+(30/2));
+    expect(draw).toHaveProperty('y', 10+(40/2));
+})
