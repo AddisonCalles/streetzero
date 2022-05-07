bc218e3ed3b901f39cca39a0a1c3e497f555543f
Addison Calles <addison.calles@clearcheck.us>
2022-05-07 10:06:01 -0500

    Alpha- CLI

---
+++ templates/base/webpack.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+
+export default {
+    entry: './src/main.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'street.game.build.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+};
