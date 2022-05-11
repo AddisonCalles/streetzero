dec886d286c87fd45f86e1f3ac7d9bfb8d4542a0
Addison Calles <addison.calles@clearcheck.us>
2022-05-10 20:17:51 -0500

    V0.6.0-alpha.7

--- src/cli/menus/main.menu.ts
+++ src/cli/menus/main.menu.ts
@@ -1,6 +1,7 @@
 import { prompt, QuestionCollection } from 'inquirer';
 import 'colors';
 import { Action } from '../actions';
+const pjson = require('../../../package.json');
 
 const options: QuestionCollection = {
   type: 'list',
@@ -22,6 +23,7 @@ export const menuActions = async () => {
   console.clear();
   console.log('===================================='.green);
   console.log('STREET ZERO - MAIN MENU');
+  console.log('Version: ', pjson.version.blue);
   console.log('===================================='.green);
   const response = await prompt(options);
   return response.option;
